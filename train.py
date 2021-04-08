b1a563a4f3663ee65b4d744199a1e6aeb2fc9554
Musa Çam <musacamm@gmail.com>
2021-04-08 16:58:06 +0300

    Create train.py

---
+++ train.py
@@ -0,0 +1,57 @@
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+# Initializing the CNN
+classifier = Sequential()
+# First convolution layer and pooling
+classifier.add(Convolution2D(32, (3, 3), input_shape=(64, 64, 1), activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+# Second convolution layer and pooling
+classifier.add(Convolution2D(32, (3, 3), activation='relu'))
+# input_shape is going to be the pooled feature maps from the previous convolution layer
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+# Flattening the layers
+classifier.add(Flatten())
+# Adding a fully connected layer
+classifier.add(Dense(units=128, activation='relu'))
+classifier.add(Dense(units=7, activation='softmax')) # softmax for more than 2
+# Compiling the CNN
+classifier.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+training_set = train_datagen.flow_from_directory('data/train',
+                                                 target_size=(64, 64),
+                                                 batch_size=5,
+                                                 color_mode='grayscale',
+                                                 class_mode='categorical')
+
+test_set = test_datagen.flow_from_directory('data/test',
+                                            target_size=(64, 64),
+                                            batch_size=5,
+                                            color_mode='grayscale',
+                                            class_mode='categorical')
+classifier.fit(
+        training_set,
+        steps_per_epoch=2100, # No of images in training set : 0 to 6 every folder has 300 images.
+        epochs=10,
+        validation_data=test_set,
+        validation_steps=70) # No of images in test set : 0 to 6 every folder has 10 images.
+
+# Saving the model
+model_json = classifier.to_json()
+with open("model-bw.json", "w") as json_file:
+    json_file.write(model_json)
+classifier.save_weights('model-bw.h5')
+
