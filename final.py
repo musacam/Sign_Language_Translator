b0181531289ce0ed6e14ca6084b6f790a9e8f71e
Musa Çam <musacamm@gmail.com>
2021-04-12 22:13:31 +0300

    Warning azaltmak için

--- final.py
+++ final.py
@@ -12,7 +12,9 @@ loaded_model.load_weights("model-bw.h5")
 cap = cv2.VideoCapture(0)
 
 # Numbers dictionary
-categories = {0: 'ZERO', 1: 'ONE', 2: 'TWO', 3: 'THREE', 4: 'FOUR', 5: 'FIVE', 6: 'SIX', 7: 'SEVEN', 8: 'EIGHT', 9: 'NINE'}
+categories = {0: 'ZERO', 1: 'ONE', 2: 'TWO', 3: 'THREE', 4: 'FOUR',
+              5: 'FIVE', 6: 'SIX', 7: 'SEVEN', 8: 'EIGHT', 9: 'NINE'}
+
 
 while True:
     _, frame = cap.read()
